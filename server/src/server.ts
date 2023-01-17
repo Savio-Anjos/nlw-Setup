ea232b0dc27580fd2ebab59bcaa3ccc95ffe2623
unknown <Savioanjos2020@gmail.com>
2023-01-17 09:41:22 -0300

    Finalizando back-end aula 1

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,7 @@ import cors from "@fastify/cors"
 import { PrismaClient } from '@prisma/client'
 
 const app = Fastfy() 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient()    
 
 app.register(cors)
 
