05cf6965740abf648dd2a78ef340c4e5d6e2e81a
unknown <Savioanjos2020@gmail.com>
2023-01-18 11:53:58 -0300

    Criando rota para criar habit

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,7 @@
 import { FastifyInstance } from "fastify"
+import { z } from "zod"
 import { prisma } from "./lib/prisma"
+import dayjs from "dayjs"
 
 
 
@@ -8,8 +10,30 @@ export async function appRoutes(app: FastifyInstance) {
   Método HTTP: Get, post, Put, PAtch, Delete
 */
 
-app.get('/hello', async () => {
-    const habits = await prisma.habit.findMany()
-    return habits
+app.post('/habits', async (request) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(
+        z.number().min(0).max(6))
+    })
+    
+    const { title, weekDays } = createHabitBody.parse(request.body)
+
+    const today = dayjs().startOf('day').toDate()
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: today,
+        weekDays: {
+          create: weekDays.map(weekDay => {
+            return {
+              week_day: weekDay,
+            }
+          })
+        }
+
+      }
+    })
 })
 }
