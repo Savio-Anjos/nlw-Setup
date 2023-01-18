ad76a3a678f34e38ad6dff70605323b42d0338cd
unknown <Savioanjos2020@gmail.com>
2023-01-18 20:00:36 -0300

    Criando tabela de hábitos diários

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import { Header } from './components/Header'
+import { Summary } from './components/SummaryTable'
 import './styles/global.css'
 // import { Habit } from "./components/Habit"
 
@@ -9,6 +10,7 @@ export function App() {
     <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
        
        <Header />
+       <Summary />
     </div>   
    </div>
   )
---
+++ web/src/components/SummaryTable.tsx
@@ -0,0 +1,45 @@
+import { HabitDay } from "./HabitDay"
+
+const weekDays = [
+    'D',
+    'S',
+    'T',
+    'Q',
+    'Q',
+    'S',
+    'S',
+]
+export function Summary() {
+    return (
+        <div className="w-full flex">
+            <div className="grid grid-rows-7 grid-flow-row gap-3">
+              
+            {weekDays.map((weekDay, i) => {
+                    return (
+                    <div
+                    key={`${weekDay}-${i}`} 
+                    className="text-zinc-400 text-xl h-10 font-bold flex items-center justify-center"
+                    >
+                     {weekDay}
+                  </div>
+                    )
+                })}
+            </div>
+
+            <div className="grid grid-rows-7 grid-flow-col gap-3">
+               
+               <HabitDay />
+               <HabitDay />
+               <HabitDay />
+               <HabitDay />
+               <HabitDay />
+               <HabitDay />
+               <HabitDay />
+               <HabitDay />
+               <HabitDay />
+               <HabitDay />
+            </div>
+
+        </div>
+    )
+}
