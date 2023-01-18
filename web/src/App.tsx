3114c03252f7154963256eeedc01547517e6987e
unknown <Savioanjos2020@gmail.com>
2023-01-18 18:26:39 -0300

    Criando componente de header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,28 @@
 import './styles/global.css'
-import { Habit } from "./components/Habit"
+import { Plus } from 'phosphor-react'
+// import { Habit } from "./components/Habit"
 
-function App() {
+import logoImage from './assets/logo.svg'
+
+export function App() {
 
   return (
-   <div>
-   <Habit completed={3} />
+   <div className="w-screen h-screen flex justify-center items-center">
+    <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+     
+     <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
+         <img src={logoImage} alt="Habits" />
+         <button 
+         type='button'
+         className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'
+         
+         >
+          <Plus size={20} className="text-violet-500"/>
+          Novo hábito
+         </button>
+     </div>
 
-   <Habit completed={10} />
-
-   <Habit completed={20} />
-
-   <Habit completed={30} />
+    </div>   
    </div>
   )
 }
-
-export default App
