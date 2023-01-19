eaf298d31224943c05da6547dfa37c9723010246
unknown <Savioanjos2020@gmail.com>
2023-01-19 10:58:37 -0300

    Criando rota de completar hábito

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -80,4 +80,37 @@ app.get('/day', async (request) => {
   }
 })
 
+app.patch('/habits/:id/toggle', async (request) => {
+
+  const toggleHabitParams = z.object({
+    id: z.string().uuid(),
+  })
+
+  const { id } = toggleHabitParams.parse(request.params)
+
+  const today = dayjs().startOf('day').toDate()
+
+  let day = await prisma.day.findUnique({
+    where: {
+      date: today,
+    }
+  })
+
+  if (!day) {
+    day = await prisma.day.create({
+      data: {
+        date: today,
+      }
+    })
+  }
+
+  await prisma.dayHabit.create({
+      data: {
+        day_id: day.id,
+        habit_id: id,
+      }
+  })
+
+})
+
 }
