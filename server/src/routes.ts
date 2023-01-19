5f86d813e06dc971d911f31a0d9de41e8ec5b05b
unknown <Savioanjos2020@gmail.com>
2023-01-19 12:57:53 -0300

    Finalizando rota de resumo de dias

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -104,13 +104,62 @@ app.patch('/habits/:id/toggle', async (request) => {
     })
   }
 
-  await prisma.dayHabit.create({
+  const dayHabit = await prisma.dayHabit.findUnique({
+    where: {
+      day_id_habit_id: {
+        day_id: day.id,
+        habit_id: id
+      }
+    }
+  })
+
+  if(dayHabit) {
+    await prisma.dayHabit.delete({
+      where: {
+        id: dayHabit.id,
+      }
+    })
+  } else {
+    await prisma.dayHabit.create({
       data: {
         day_id: day.id,
         habit_id: id,
       }
   })
+  }
+  
+  // Completar o hábito
 
+})
+
+app.get('/summary', async () => {
+
+  const summary = await prisma.$queryRaw`
+     SELECT 
+      D.id,
+      D.date,
+      (
+        SELECT 
+          cast(count(*) as float)
+        FROM day_habits DH 
+        WHERE DH.day_id = D.id
+      ) as completed,
+      (
+        SELECT 
+          cast(count(*) as float)
+        FROM habit_week_days HWD
+        JOIN habits H
+          ON H.id = HWD.habit_id
+        WHERE
+          HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+          AND H.created_at <= D.date
+
+      ) as amount
+
+     FROM days D
+  `
+
+  return summary
 })
 
 }
