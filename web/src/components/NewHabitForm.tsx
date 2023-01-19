a281cd576a1b9a06d436354bc7511a952a300988
unknown <Savioanjos2020@gmail.com>
2023-01-19 17:32:03 -0300

    Finalizando modal

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -2,8 +2,8 @@ import { Check } from "phosphor-react";
 
 export function NewHabitForm() {
     return (
-        <form>
-            <label htmlFor="title">
+        <form className="w-full flex flex-col mt-6">
+            <label htmlFor="title" className="font-semibold leading-tight">
                 Qual seu comprometimento?
             </label>
 
@@ -11,14 +11,15 @@ export function NewHabitForm() {
               type="text"
               id="title"
               placeholder="Exexcícios, dormir bem, etc..."
+              className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
               autoFocus
             />
 
-            <label htmlFor="">
+            <label htmlFor="" className="font-semibold leading-tight mt-4">
                 Qual a recorrência?
             </label>
 
-            <button type="submit">
+            <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500">
                 <Check size={20} weight="bold" />
                 Confirmar
             </button>
