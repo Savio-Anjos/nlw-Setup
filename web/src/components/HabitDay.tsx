b7c6b3a99ff690123a2c7215848b0ba79f9b9770
unknown <Savioanjos2020@gmail.com>
2023-01-19 19:15:13 -0300

    Finalizando componente papover

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,4 +1,5 @@
 import * as Popover from '@radix-ui/react-popover';
+import { ProgressBar } from './ProgressBar';
 //interface HabitDayProps {
   //  completed: number
 //}
@@ -9,7 +10,10 @@ export function HabitDay() {
       <Popover.Root>
         <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
         <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col' >
-             resteeee
+             <span className='font-semibold text-zinc-400'>terça-feira</span>
+             <span className='mt-1 font-extrabold leading-tight text-3xl'>17/01</span>
+
+             <ProgressBar progress={75} />
 
              <Popover.Arrow height={8} width={16} className='fill-zinc-900'/>
         </Popover.Content>
