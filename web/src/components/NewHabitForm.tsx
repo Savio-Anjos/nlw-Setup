c138255e74a6056d97e9fb991d2104b02471487b
unknown <Savioanjos2020@gmail.com>
2023-01-22 04:45:28 -0300

    Criando checkbox

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,4 +1,15 @@
+import * as Checkbox from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
+
+const availableWeekDays = [
+  'Domingo',
+  'Segunda-feira',
+  'Terça-feira',
+  'Quarta-feira',
+  'Quinta-feira',
+  'Sexta-feira',
+  'Sábado',
+]
 
 
 export function NewHabitForm() {
@@ -19,6 +30,30 @@ export function NewHabitForm() {
             <label htmlFor="" className="font-semibold leading-tight mt-4">
                 Qual a recorrência?
             </label>
+
+            <div className="flex flex-col gap-2 mt-3">
+              {availableWeekDays.map(weekDay => {
+                return (
+                   <Checkbox.Root
+                   key={weekDay}
+                   className='flex items-center gap-3 group'
+                >
+                   <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+                   <Checkbox.Indicator>
+                    <Check size={20} className="text-white"/>
+                   </Checkbox.Indicator>
+                   </div>
+  
+                  <span className=' text-white leading-tight '>
+                    {weekDay}
+                  </span>
+  
+                </Checkbox.Root>
+                )
+
+                })}
+         
+            </div>
 
             <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500">
                 <Check size={20} weight="bold" />
