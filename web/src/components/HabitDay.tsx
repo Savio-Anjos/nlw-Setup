9338d5eeb9140638364fa767ab47cdcf9dd9697b
unknown <Savioanjos2020@gmail.com>
2023-01-22 06:05:32 -0300

    Utilizando dados da api no papover

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -3,6 +3,7 @@ import * as Checkbox from '@radix-ui/react-checkbox';
 import clsx from 'clsx'
 import { ProgressBar } from './ProgressBar';
 import { Check } from 'phosphor-react';
+import dayjs from 'dayjs';
 
 interface HabitDayProps {
   date: Date;
@@ -10,8 +11,11 @@ interface HabitDayProps {
    amount?: number;
 }
 
-export function HabitDay({ completed = 0, amount = 0 }: HabitDayProps ) {
+export function HabitDay({ completed = 0, amount = 0, date }: HabitDayProps ) {
   const completedPercentage = amount > 0 ? Math.round((completed / amount) * 100) : 0
+
+   const dayAndMonth = dayjs(date).format('DD/MM')
+   const dayOfWeek = dayjs(date).format('dddd')
   
     return (
       <Popover.Root>
@@ -25,8 +29,8 @@ export function HabitDay({ completed = 0, amount = 0 }: HabitDayProps ) {
            })} />
 
         <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col' >
-             <span className='font-semibold text-zinc-400'>terça-feira</span>
-             <span className='mt-1 font-extrabold leading-tight text-3xl'>17/01</span>
+             <span className='font-semibold text-zinc-400'>{dayOfWeek}</span>
+             <span className='mt-1 font-extrabold leading-tight text-3xl'>{dayAndMonth}</span>
 
              <ProgressBar progress={completedPercentage} />
 
